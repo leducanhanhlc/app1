7052e0f327594c5a5ce06bf9603bd065520a047e
ducanh <leducanhanhlc@gmail.com>
2017-02-10 20:23:02 +0700

    up image from file
    1. copy to drawable
    2. setImageResource

--- MyApplication/app/src/main/java/com/example/admin/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/admin/myapplication/MainActivity.java
@@ -1,18 +1,27 @@
 package com.example.admin.myapplication;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.Drawable;
 import android.hardware.camera2.params.BlackLevelPattern;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 
+import java.io.File;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ImageView imageview = (ImageView) findViewById(R.id.imageview);
         setContentView(R.layout.activity_main);
+
+
+        ImageView imageView = (ImageView) findViewById(R.id.imageview);
+
+        imageView.setImageResource(R.drawable.download);
 
     }
 }
