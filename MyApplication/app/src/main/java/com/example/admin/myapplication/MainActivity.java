c354a3dd97f53c6ac083c9e90f3225985aaae4a5
ducanh <leducanhanhlc@gmail.com>
2017-02-14 19:22:49 +0700

    multi activity success
    multi activity success

--- MyApplication/app/src/main/java/com/example/admin/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/admin/myapplication/MainActivity.java
@@ -1,12 +1,18 @@
 package com.example.admin.myapplication;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.drawable.Drawable;
 import android.hardware.camera2.params.BlackLevelPattern;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 
 import java.io.File;
@@ -18,5 +24,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getSupportActionBar().hide();
+        final Button BUY = (Button) findViewById(R.id.BUY);
+        final Button SELL = (Button) findViewById(R.id.SELL);
+        show(BUY);
+        show(SELL);
+    }
+
+    public void show(final View view) {
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, buy.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
