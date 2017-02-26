3629755554d5618fbf1ae719a5aa4ff62fa3daba
ducanh <leducanhanhlc@gmail.com>
2017-02-26 17:13:03 +0700

    add toast database
    add toast database

--- MyApplication/app/src/main/java/com/example/admin/myapplication/VIEW/ADDSHOP/AddshopView.java
+++ MyApplication/app/src/main/java/com/example/admin/myapplication/VIEW/ADDSHOP/AddshopView.java
@@ -4,7 +4,10 @@ import android.content.Context;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.admin.myapplication.PRESENTER.ADDSHOP.Addshoppre;
 import com.example.admin.myapplication.R;
@@ -20,12 +23,20 @@ public class AddshopView extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.addshop);
 
-        EditText ID = (EditText) findViewById(R.id._ID);
-        EditText add_editText = (EditText) findViewById(R.id.address);
-        EditText type_editText = (EditText) findViewById(R.id.type);
-        EditText item_editText = (EditText) findViewById(R.id.item);
+        final EditText ID = (EditText) findViewById(R.id._ID);
+        final EditText add_editText = (EditText) findViewById(R.id.address);
+        final EditText type_editText = (EditText) findViewById(R.id.type);
+        final EditText item_editText = (EditText) findViewById(R.id.item);
 
-        Addshoppre addshoppre = new Addshoppre(getApplication().getApplicationContext());
-        addshoppre.add(ID, add_editText, type_editText, item_editText);
+        final Addshoppre addshoppre = new Addshoppre(getApplication().getApplicationContext());
+        Button button_add = (Button) findViewById(R.id.add_button);
+        button_add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addshoppre.add(ID, add_editText, type_editText, item_editText);
+                Toast toast = Toast.makeText(getApplicationContext(), "saved", Toast.LENGTH_LONG);
+                toast.show();
+            }
+        });
     }
 }
